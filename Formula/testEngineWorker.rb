78e17a14e9e2b4ba4c182095ef7f6c56c653e876
apimation <dev@apimation.com>
2018-05-03 23:36:16 +0300

    testEngineWorker version v0.4.18

--- Formula/testEngineWorker.rb
+++ Formula/testEngineWorker.rb
@@ -1,9 +1,9 @@
 class TestEngineWorker < Formula
   desc "apimation test engine worker"
   homepage "https://apimation.com/"
-  url "https://github.com/apimation/testEngineWorker/releases/download/v0.4.17/testEngineWorker_0.4.17_macOS_amd64.zip"
-  version "0.4.17"
-  sha256 "fcaf78ee426cf73fe11436c31489984db3a66ed733a8bca8718f24ad65e17e6a"
+  url "https://github.com/apimation/testEngineWorker/releases/download/v0.4.18/testEngineWorker_0.4.18_macOS_amd64.zip"
+  version "0.4.18"
+  sha256 "1cc18bfe878db3f36b02c74f80705ed50c61b6a2c5ab2731923d856d1033d43d"
   
   depends_on "git"
 
