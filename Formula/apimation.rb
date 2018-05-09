7ee741accb920ac738bbe3a7d868f265d5559bfe
apimation <dev@apimation.com>
2018-05-09 21:50:54 +0300

    apimation version v0.23.16

--- Formula/apimation.rb
+++ Formula/apimation.rb
@@ -1,9 +1,9 @@
 class Apimation < Formula
   desc "apimation cli client"
   homepage "https://apimation.com/"
-  url "https://github.com/apimation/cli-client/releases/download/v0.23.0/apimation_0.23.0_macOS_amd64.zip"
-  version "0.23.0"
-  sha256 "a28d279301dedff1c39c506ee21ba0b617675039d923cec85e29d387eda8f53b"
+  url "https://github.com/apimation/cli-client/releases/download/v0.23.16/apimation_0.23.16_macOS_amd64.zip"
+  version "0.23.16"
+  sha256 "851b0680c489a50a1df930aa480811f2712f65b03b23a437a61c02383b7ca772"
   
   depends_on "git"
 
