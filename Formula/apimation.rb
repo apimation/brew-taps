2a01efbe183ce457c78fdf5f115152c0daf0f57b
apimation <dev@apimation.com>
2018-10-05 16:55:45 +0300

    Brew formula update for apimation version v0.41.3

--- Formula/apimation.rb
+++ Formula/apimation.rb
@@ -1,9 +1,9 @@
 class Apimation < Formula
   desc "apimation cli client"
   homepage "https://apimation.com/"
-  url "https://github.com/apimation/cli-client/releases/download/v0.41.2/apimation_0.41.2_macOS_amd64.zip"
-  version "0.41.2"
-  sha256 "768f3a7596da8a96562669ee4c971b020ebb7974c5c98339a5387362b70a6050"
+  url "https://github.com/apimation/cli-client/releases/download/v0.41.3/apimation_0.41.3_macOS_amd64.zip"
+  version "0.41.3"
+  sha256 "4cfa27e22c70548c8008f02a21913b96458baa7ef042cd672c0ba30940117a3f"
   
   depends_on "git"
 
