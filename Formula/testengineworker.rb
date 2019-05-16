e3e7799cc03a396122cac817d600715e7432a8ce
apimation <dev@apimation.com>
2019-05-16 14:50:11 +0300

    Brew formula update for testengineworker version v0.24.9

--- Formula/testengineworker.rb
+++ Formula/testengineworker.rb
@@ -1,9 +1,9 @@
 class Testengineworker < Formula
   desc "apimation test engine worker"
   homepage "https://apimation.com/"
-  url "https://github.com/apimation/testEngineWorker/releases/download/v0.24.8/testengineworker_0.24.8_macOS_amd64.zip"
-  version "0.24.8"
-  sha256 "235ff4a83ce0d8bc01cc3a84611acb3d71cd88eb04bf24cca6eaaaf22d5b56aa"
+  url "https://github.com/apimation/testEngineWorker/releases/download/v0.24.9/testengineworker_0.24.9_macOS_amd64.zip"
+  version "0.24.9"
+  sha256 "a8f5c579e07f78d0eb4891a289e94501b19ec222b08632c1a745c33b9d0893ac"
   
   depends_on "git"
 
